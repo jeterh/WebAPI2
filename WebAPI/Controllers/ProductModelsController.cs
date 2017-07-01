da17d5d03efac3f73de06ef5c94941293fb921a2
Jeter <jeterh@gmail.com>
2017-07-01 09:58:33 +0800

    1.加入 .gitignore 與 .gitattributes。 2.加入專案檔案。 3.02 建立 ASP .NET Web API 專案範本、加入範例資料庫、新增 ADO .NET 實體資料模型 4.加入 ProductsController 5.03 建立 ProductsController 並透過 Postman 成功呼叫 API

---
+++ WebAPI/Controllers/ProductModelsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ProductModelsController : ApiController
+    {
+       static List<ProductModel> products = new List<ProductModel>
+               {
+            new ProductModel { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new ProductModel { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new ProductModel { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+               };
+
+        public IEnumerable<ProductModel> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        public void PostProduct(ProductModel product)
+        {
+            products.Add(product);
+        }
+
+    }
+}
